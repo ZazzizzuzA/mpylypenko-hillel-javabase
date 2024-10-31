d0a837829ab4507940eca9ec75fe358a6abaa487
zazzizzuza <musicvyp@gmail.com>
2024-10-31 12:04:05 +0200

    change message about shop

--- 3.1/src/com/gmail/mpylypenko/gitpractice/shop/ShopOnline.java
+++ 3.1/src/com/gmail/mpylypenko/gitpractice/shop/ShopOnline.java
@@ -2,6 +2,6 @@ package com.gmail.mpylypenko.gitpractice.shop;
 
 public class ShopOnline {
     public static void main(String[] args) {
-        System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
     }
 }

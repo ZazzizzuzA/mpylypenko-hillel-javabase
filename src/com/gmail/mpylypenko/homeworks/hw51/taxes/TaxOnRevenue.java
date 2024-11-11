2ac3c163e7dd7c5e9c4bfdd14bc1b061b7020fa4
Mykhailo Pylypenko <musicvyp@gmail.com>
2024-11-11 18:10:19 +0200

    work with comments from pull request

--- src/com/gmail/mpylypenko/homeworks/hw51/taxes/TaxOnRevenue.java
+++ src/com/gmail/mpylypenko/homeworks/hw51/taxes/TaxOnRevenue.java
@@ -3,52 +3,52 @@ package com.gmail.mpylypenko.homeworks.hw51.taxes;
 import java.util.Scanner;
 
 public class TaxOnRevenue {
-    private static double FIRST_LVL_TAXES = 2.5;
-    private static double SECOND_LVL_TAXES = 4.3;
-    private static double THIRD_LVL_TAXES = 6.7;
-    private static byte MAX_TRIES = 5;
+    private static byte maxTries = 5;
+    private static Scanner scannerState = new Scanner(System.in);
 
     public static void main(String[] args) {
         startScanner();
     }
 
-    protected static double[] taxCalculate(int revenue) {
+    protected static double[] getPercentageAndCalculatedTax(int revenue) {
+        double lowTaxRate = 2.5;
+        double middleTaxRate = 4.3;
+        double highTaxRate = 6.7;
         double[] result = new double[2];
         if (revenue <= 10_000) {
-            result[0] = FIRST_LVL_TAXES;
-            result[1] = revenue * (FIRST_LVL_TAXES / 100);
+            result[0] = lowTaxRate;
+            result[1] = taxCalculate(revenue, lowTaxRate);
         } else if (revenue <= 25_000) {
-            result[0] = SECOND_LVL_TAXES;
-            result[1] = revenue * (SECOND_LVL_TAXES / 100);
+            result[0] = middleTaxRate;
+            result[1] = taxCalculate(revenue, middleTaxRate);
         } else {
-            result[0] = THIRD_LVL_TAXES;
-            result[1] = revenue * (THIRD_LVL_TAXES / 100);
+            result[0] = highTaxRate;
+            result[1] = taxCalculate(revenue, highTaxRate);
         }
         return result;
     }
 
-    private static Scanner scannerState() {
-        return new Scanner(System.in);
+    protected static double taxCalculate(int revenue, double percentage) {
+        return revenue * (percentage / 100);
     }
 
     private static void startScanner() {
-        Scanner scanner = scannerState();
         System.out.println("Please, enter your revenue: ");
-        int revenueValue = scanner.nextInt();
+        int revenueValue = scannerState.nextInt();
 
-        if (revenueValue < 0 && MAX_TRIES > 1) {
+        if (revenueValue < 0 && maxTries > 1) {
             System.out.println("Revenue can not be less, than 1. Please, try again.");
-            MAX_TRIES--;
+            maxTries--;
             startScanner();
             return;
-        } else if (MAX_TRIES <= 1) {
+        } else if (maxTries <= 1) {
             System.out.println("Revenue can not be less, than 1. You've done 5 tries! That's enough. I'm quit!");
             return;
         }
 
-        double[] calculatedTax = taxCalculate(revenueValue);
-        System.out.printf("Taxes on your revenue with percentage %.2f", calculatedTax[0]);
-        System.out.printf(" is %.2f", calculatedTax[1]);
+        double[] calculatedTaxAndPercentage = getPercentageAndCalculatedTax(revenueValue);
+        System.out.printf("Tax on your revenue with percentage %.2f", calculatedTaxAndPercentage[0]);
+        System.out.printf(" is %.2f", calculatedTaxAndPercentage[1]);
 
     }
 }

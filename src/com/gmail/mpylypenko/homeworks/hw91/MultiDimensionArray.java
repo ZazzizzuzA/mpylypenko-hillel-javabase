852bb2ff7cdf9c1ab4df0b74d818984ffbdbe78f
zazzizzuza <musicvyp@gmail.com>
2024-11-26 22:58:45 +0200

    homework 9.1: rewrite check Magical square; work with comments

--- src/com/gmail/mpylypenko/homeworks/hw91/MultiDimensionArray.java
+++ src/com/gmail/mpylypenko/homeworks/hw91/MultiDimensionArray.java
@@ -12,11 +12,14 @@ public class MultiDimensionArray {
 
         int[][] matrix = generateMatrix(axisX, axisY, maxValueOfMatrix);
         printMatrix(matrix);
-        sumValuesRowsByTypeAndPrint(matrix, "odd");
-        sumValuesRowsByTypeAndPrint(matrix, "even");
 
-        multiplyValuesInColumnsAndPrint(matrix, "odd");
-        multiplyValuesInColumnsAndPrint(matrix, "even");
+        int[] arraySumsEvenAndOddValues = sumsValuesRowsByType(matrix);
+
+        printInfo("Sum of all values in each even rows is " + arraySumsEvenAndOddValues[0], true);
+        printInfo("Sum of all values in each odd rows is " + arraySumsEvenAndOddValues[1], false);
+        int[] arrayMultipliesEvenAndOddValues = multiplyValuesInColumnsByType(matrix);
+        printInfo("Multiply of all values in each even column is " + arrayMultipliesEvenAndOddValues[0], true);
+        printInfo("Multiply of all values in each odd column is " + arrayMultipliesEvenAndOddValues[1], false);
 
         checkOnMagicMatrixAndPrintResult(matrix);
     }
@@ -48,89 +51,137 @@ public class MultiDimensionArray {
         }
     }
 
-    private static void sumValuesRowsByTypeAndPrint(int[][] matrix, String type) {
-        System.out.print("Sum of all values in an " + type + " rows (row ");
-        int sumValuesOfRow = 0;
+    private static int[] sumsValuesRowsByType(int[][] matrix) {
+        int[] arraySumsEvenAndOddValues = new int[2];
         for (int i = 0; i < matrix.length; i++) {
-            boolean check = type == "odd" ? i % 2 == 0 : i % 2 != 0;
-            if (check) {
-                continue;
-            }
-            System.out.print(i + " ");
-            for (int columns : matrix[i]) {
-                sumValuesOfRow += columns;
+            for (int cell : matrix[i]) {
+                arraySumsEvenAndOddValues[i % 2] += cell;
             }
         }
-        System.out.print(") ");
-        System.out.println("is " + sumValuesOfRow + ";");
+        return arraySumsEvenAndOddValues;
     }
 
-    private static void multiplyValuesInColumnsAndPrint(int[][] matrix, String type) {
-        System.out.print("Multiply of all values in an " + type + " columns (column ");
-        int multiplyValuesOfRow = 0;
-        StringBuilder columns = new StringBuilder();
+    private static int[] multiplyValuesInColumnsByType(int[][] matrix) {
+        int[] multiplyEvenAndOddValuesOfRow = new int[2];
         for (int[] row : matrix) {
             for (int j = 0; j < row.length; j++) {
-                boolean check = type == "odd" ? j % 2 == 0 : j % 2 != 0;
-                if (check) {
-                    continue;
-                }
-                if (!columns.toString().contains("" + j)) {
-                    columns.append(j).append(" ");
-                }
-
-                if (multiplyValuesOfRow == 0) {
-                    multiplyValuesOfRow = row[j];
+                int indexByType = j % 2;
+                if (multiplyEvenAndOddValuesOfRow[indexByType] == 0) {
+                    multiplyEvenAndOddValuesOfRow[indexByType] = row[j];
                 } else {
-                    multiplyValuesOfRow = multiplyValuesOfRow * row[j];
+                    multiplyEvenAndOddValuesOfRow[indexByType] = multiplyEvenAndOddValuesOfRow[indexByType] * row[j];
                 }
             }
         }
-        System.out.print(columns);
-        System.out.print(") ");
-        System.out.println("is " + multiplyValuesOfRow + ";");
+        return multiplyEvenAndOddValuesOfRow;
     }
 
     private static void checkOnMagicMatrixAndPrintResult(int[][] matrix) {
+        printInfo("", true);
         if (matrix.length != matrix[0].length) {
-            System.out.println("Matrix is not a Magical square.");
+            printInfo("Matrix is not a Magical square.", false);
             return;
         }
 
-        int[] sumValuesOfEachRow = new int[matrix.length];
-        int[] sumValuesOfEachColumn = new int[matrix[0].length];
-        int[] sumValuesOfEachDiagonals = new int[2];
-        int firstSumForCheck = 0;
+        /* Second try */
 
+        int totalSum = 0;
+        int firstSum = 0;
+
+        /*Sum values of row cells*/
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[i].length; j++) {
+                totalSum += matrix[i][j];
+            }
+            /*Defined first value of sum; other must be equal*/
+            if (firstSum == 0) {
+                firstSum = totalSum;
+            }
+            if (i == matrix.length - 1 && firstSum != totalSum / matrix.length) {
+                printInfo("Sums of each row is not equal. Expected average from all rows: " + firstSum + "; received: " + totalSum / matrix.length, true);
+                printInfo("Matrix is not a Magical square.", false);
+                return;
+            }
+
+        }
+
+        /*Sum values of column cells*/
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                sumValuesOfEachRow[i] += matrix[i][j];
-                sumValuesOfEachColumn[j] += matrix[i][j];
+                totalSum += matrix[j][i];
+            }
+            if (i == matrix.length - 1 && firstSum != totalSum / (matrix.length + matrix[i].length)) {
+                printInfo("Sums of each column is not equal. Expected from all columns: " + firstSum + "; received: " + totalSum / (matrix.length + matrix[i].length), true);
+                printInfo("Matrix is not a Magical square.", false);
+                return;
+            }
+
+        }
+
+        /*Sum values of diagonals cells*/
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
                 if (i == j) {
-                    sumValuesOfEachDiagonals[0] += matrix[i][j];
+                    totalSum += matrix[i][j];
                 }
                 if (matrix[i].length - 1 - i == j) {
-                    sumValuesOfEachDiagonals[1] += matrix[i][j];
+                    totalSum += matrix[i][j];
                 }
-
-                if (j == matrix[i].length - 1 && i == 0) {
-                    firstSumForCheck = sumValuesOfEachRow[i];
-                }
-
-                if (j == matrix[i].length - 1 && i == matrix.length - 1) {
-                    if (sumValuesOfEachRow[i] != firstSumForCheck || sumValuesOfEachColumn[j] != firstSumForCheck || sumValuesOfEachDiagonals[0] != firstSumForCheck || sumValuesOfEachDiagonals[1] != firstSumForCheck) {
-                        System.out.println("Sums of each row is " + Arrays.toString(sumValuesOfEachRow));
-                        System.out.println("Sums of each column is " + Arrays.toString(sumValuesOfEachColumn));
-                        System.out.println("Sums of each diagonals is " + Arrays.toString(sumValuesOfEachDiagonals));
-                        System.out.println("Matrix is not a Magical square.");
-                        return;
-                    }
+            }
+            if (i == matrix.length - 1) {
+                if (firstSum != totalSum / (matrix.length + matrix[i].length + 2)) {
+                    printInfo("Sums of each diagonal is not equal. Expected from all diagonals: " + firstSum + "; received: " + totalSum / (matrix.length + matrix[i].length + 2), true);
+                    printInfo("Matrix is not a Magical square.", false);
+                    return;
                 }
             }
         }
-        System.out.println("Sums of each row is " + Arrays.toString(sumValuesOfEachRow));
-        System.out.println("Sums of each column is " + Arrays.toString(sumValuesOfEachColumn));
-        System.out.println("Sums of each diagonals is " + Arrays.toString(sumValuesOfEachDiagonals));
-        System.out.println("Matrix is a Magical square.");
+
+        /* First try */
+
+//        int[] sumValuesOfEachRow = new int[matrix.length];
+//        int[] sumValuesOfEachColumn = new int[matrix[0].length];
+//        int[] sumValuesOfEachDiagonals = new int[2];
+//        int firstSumForCheck = 0;
+
+//        for (int i = 0; i < matrix.length; i++) {
+//            for (int j = 0; j < matrix[i].length; j++) {
+//                sumValuesOfEachRow[i] += matrix[i][j];
+//                sumValuesOfEachColumn[j] += matrix[i][j];
+//                if (i == j) {
+//                    sumValuesOfEachDiagonals[0] += matrix[i][j];
+//                }
+//                if (matrix[i].length - 1 - i == j) {
+//                    sumValuesOfEachDiagonals[1] += matrix[i][j];
+//                }
+//
+//                if (j == matrix[i].length - 1 && i == 0) {
+//                    firstSumForCheck = sumValuesOfEachRow[i];
+//                }
+//
+//                if (j == matrix[i].length - 1 && i == matrix.length - 1) {
+//                    if (sumValuesOfEachRow[i] != firstSumForCheck || sumValuesOfEachColumn[j] != firstSumForCheck || sumValuesOfEachDiagonals[0] != firstSumForCheck || sumValuesOfEachDiagonals[1] != firstSumForCheck) {
+//                        printInfo("Sums of each row is " + sumValuesOfEachRow[0], true);
+//                        printInfo("Sums of each column is " + sumValuesOfEachColumn[0], false);
+//                        printInfo("Sums of each diagonals is " + sumValuesOfEachDiagonals[0], false);
+//                        printInfo("Matrix is not a Magical square.", false);
+//                        return;
+//                    }
+//                }
+//            }
+//        }
+//        printInfo("Sums of each row is " + Arrays.toString(sumValuesOfEachRow), true);
+//        printInfo("Sums of each column is " + Arrays.toString(sumValuesOfEachColumn), false);
+//        printInfo("Sums of each diagonals is " + Arrays.toString(sumValuesOfEachDiagonals), false);
+        printInfo("Matrix is a Magical square.", false);
+    }
+
+    private static void printInfo(String text, boolean needDivider) {
+        if (needDivider) {
+            System.out.println("<--==========================-->");
+        }
+        System.out.println(text);
     }
 }

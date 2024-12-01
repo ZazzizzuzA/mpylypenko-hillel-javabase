0ea9747b76d97c2401923f51207b1f12848ef26e
zazzizzuza <musicvyp@gmail.com>
2024-12-01 21:20:43 +0200

    homework 10.1: work with comments 2

--- src/com/gmail/mpylypenko/homeworks/hw101/CustomMethods.java
+++ src/com/gmail/mpylypenko/homeworks/hw101/CustomMethods.java
@@ -6,35 +6,35 @@ import java.util.Scanner;
 
 public class CustomMethods {
     public static void main(String[] args) {
-        int integer = askClientEnterInteger("1) Please, enter an integer:");
-        int secondPowInteger = squareNumber(integer);
-        System.out.println("Your integer in a second power is " + secondPowInteger);
-
-        double radius = askClientEnterDouble("\n2) Please, enter a radius of cylinder:");
-        double height = askClientEnterDouble("Please, enter a height of cylinder:");
-        double cylinderVolume = calculateCylinderVolume(radius, height);
-        System.out.print("Cylinder with radius " + radius + " and height " + height);
-        System.out.println(" has a volume " + cylinderVolume);
-
-        int arrayLength = askClientEnterInteger("\n3) Please, enter a length for array:");
-        int maxValue = askClientEnterInteger("And max value for array:");
-        int[] array = generateArray(arrayLength, maxValue);
-        int sumArrayValues = sumAllArrayValues(array);
-        System.out.println("Array of numbers:" + Arrays.toString(array));
-        System.out.print("Sum of array values is " + sumArrayValues);
-
-        String text = askClientEnterText("\n4) Please, enter some text:");
-        String reverseText = reverseText(text);
-        System.out.println("Reversed text:" + reverseText);
+//        int integer = askClientEnterInteger("1) Please, enter an integer:");
+//        int secondPowInteger = squareNumber(integer);
+//        System.out.println("Your integer in a second power is " + secondPowInteger);
+//
+//        double radius = askClientEnterDouble("\n2) Please, enter a radius of cylinder:");
+//        double height = askClientEnterDouble("Please, enter a height of cylinder:");
+//        double cylinderVolume = calculateCylinderVolume(radius, height);
+//        System.out.print("Cylinder with radius " + radius + " and height " + height);
+//        System.out.println(" has a volume " + cylinderVolume);
+//
+//        int arrayLength = askClientEnterInteger("\n3) Please, enter a length for array:");
+//        int maxValue = askClientEnterInteger("And max value for array:");
+//        int[] array = generateArray(arrayLength, maxValue);
+//        int sumArrayValues = sumAllArrayValues(array);
+//        System.out.println("Array of numbers:" + Arrays.toString(array));
+//        System.out.print("Sum of array values is " + sumArrayValues);
+//
+//        String text = askClientEnterText("\n4) Please, enter some text:");
+//        String reverseText = reverseText(text);
+//        System.out.println("Reversed text:" + reverseText);
 
         int number = askClientEnterInteger("\n5) Please, enter a number:");
         int powerNumber = askClientEnterInteger("And number to power it:");
         double poweredNumber = powNumber(number, powerNumber);
         System.out.println("Result " + number + "^" + powerNumber + " is " + poweredNumber);
 
-        int repeats = askClientEnterInteger("\n6) Please, enter a amount of repeats:");
-        String textToRepeat = askClientEnterText("And text to repeat:");
-        print(textToRepeat, repeats);
+//        int repeats = askClientEnterInteger("\n6) Please, enter a amount of repeats:");
+//        String textToRepeat = askClientEnterText("And text to repeat:");
+//        print(textToRepeat, repeats);
     }
 
     private static void print(String text, int repeats) {
@@ -44,14 +44,13 @@ public class CustomMethods {
     }
 
     private static double powNumber(int number, int powerNumber) {
-        int poweredNumber = number;
+        double poweredNumber = number;
         boolean isNegativePower = powerNumber < 0;
-        for (int i = isNegativePower ? powerNumber * -1 : powerNumber; i > 1; i--) {
+        for (int i = Math.abs(powerNumber); i > 1; i--) {
             poweredNumber = poweredNumber * number;
         }
-        System.out.println(poweredNumber);
         /* or can be used Math.pow(number, powerNumber) */
-        return isNegativePower ? (double) 1 / poweredNumber : poweredNumber;
+        return isNegativePower ? 1 / poweredNumber : poweredNumber;
     }
 
     private static int squareNumber(int integer) {
@@ -72,9 +71,8 @@ public class CustomMethods {
 
     private static String reverseText(String text) {
         String reverseText = "";
-        char[] arrayChars = text.toCharArray();
-        for (int i = arrayChars.length - 1; i >= 0; i--) {
-            reverseText += arrayChars[i];
+        for (int i = text.length() - 1; i >= 0; i--) {
+            reverseText += text.charAt(i);
         }
         return reverseText;
     }
